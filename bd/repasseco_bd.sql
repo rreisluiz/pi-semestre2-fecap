a34a9d4db187dce695550c523ff88c35768e8e2b
Vitoria <vitoriamaciel110206@gmail.com>
2024-11-18 22:20:40 -0300

    Atualização do Banco

--- bd/repasseco_bd.sql
+++ bd/repasseco_bd.sql
@@ -7,19 +7,17 @@ nome_usuario varchar(100) not null,
 email_usuario varchar(100) not null,
 senha_usuario varchar(100) not null,
 data_nascimento_usuario date not null,
-CEP char(15) not null,
-logradouro varchar(200) not null,
-EnderecoNumero varchar(10) not null,
-bairro varchar(150) not null,
-uf char(2) not null,
-cidade varchar(50) not null,
-telefone varchar(20) not null,
+logradouro varchar(200),
+complemento varchar(50),
+bairro varchar(150),
+uf char(2),
+cidade varchar(50),
+telefone varchar(20),
 constraint PKcpf primary key(CPF)
 );
 
 create table item(
 id_item int auto_increment,
-foto_item varchar(250) not null,
 descricao_item varchar(5000) not null,
 nome_item varchar (200) not null,
 categoria_item varchar (100) not null,
@@ -27,4 +25,32 @@ estado_uso_item varchar(100) not null,
 CPF char(11) not null,
 constraint PKitem primary key(id_item),
 constraint FKcpf foreign key(CPF) references usuario(CPF)
+);
+
+create table imagem(
+	id_foto int auto_increment,
+    foto varchar(5000),
+    id_item int,
+    constraint PKid primary key(id_foto),
+	constraint FKid_item foreign key(id_item) references item(id_item)
+);
+
+create table interesse(
+id_interesse int auto_increment,
+data_interesse date not null,
+CPF char(11) not null,
+id_item int,
+constraint PKinteresse primary key(id_interesse),
+constraint FKcpf_interesse foreign key(CPF) references usuario(CPF),
+constraint FKitem_interesse foreign key(id_item) references item(id_item)
+);
+
+create table possuir(
+id_possuir int auto_increment,
+data_possuir date not null,
+id_interesse int,
+id_item int,
+constraint PKpossuir primary key(id_possuir),
+constraint FKinteresse foreign key(id_interesse) references interesse(id_interesse),
+constraint FKitem foreign key(id_item) references item(id_item)
 );
